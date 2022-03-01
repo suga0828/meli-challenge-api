d109a24dc9c184a9cd811daaf38c71144d331512
Alexander Sandoval <alexander.sf0828@gmail.com>
2022-02-28 23:57:35 -0500

    test: add jest

---
+++ src/utils/author.util.spec.ts
@@ -0,0 +1,17 @@
+import { getAuthor } from "./author.util";
+
+describe('getAuthor', () => {
+  it('should return author from input', () => {
+    const headers = {
+      name: 'name',
+      lastname: 'lastname',
+    };
+
+    const author = getAuthor(headers);
+
+    expect(author).toEqual({
+      name: headers.name,
+      lastname: headers.lastname,
+    });
+  })
+})
