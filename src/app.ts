e2ba0eb96aa9a3fca09f2554eee8c7b88d761f17
Alexander Sandoval <alexander.sf0828@gmail.com>
2022-03-04 00:15:35 -0500

    fix: refactor app as module

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import cors from 'cors';
+
+import product from './product';
+import products from './products';
+
+const app = express()
+
+const whitelist = ["http://localhost:3000", "https://suga0828-meli-challenge.vercel.app"]
+const corsOptions = {
+  origin: (origin: any, callback: any) => {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error("Not allowed by CORS"))
+    }
+  },
+  credentials: true,
+}
+app.use(cors(corsOptions))
+
+app.use(express.json());
+
+app.get('/api/items/:id', product);
+app.get('/api/items', products);
+
+export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,6 @@
-import express from 'express';
-import cors from 'cors';
+import app from "./app"
 
-import product from './product';
-import products from './products';
-
-const app = express()
 const port = process.env.PORT || 3001
-
-const whitelist = ["http://localhost:3000", "https://suga0828-meli-challenge.vercel.app"]
-const corsOptions = {
-  origin: (origin: any, callback: any) => {
-    if (!origin || whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error("Not allowed by CORS"))
-    }
-  },
-  credentials: true,
-}
-app.use(cors(corsOptions))
-
-app.use(express.json());
-
-app.get('/api/items/:id', product);
-app.get('/api/items', products);
 
 app.listen(port, () => {
   console.log(`meli-challenge-api app listening on port ${port}`)
